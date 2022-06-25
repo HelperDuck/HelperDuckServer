b2bdfa5020ff108b482664f83662240f8140b960
Mauricio Scain <scainmauricio@gmail.com>
2022-06-25 11:19:45 +0200

    typecript express running

---
+++ build/index.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+// import DotEnv from "dotenv";
+// import Cors from "cors";
+const app = (0, express_1.default)();
+const port = process.env.PORT || 3002;
+app.get("/", (req, res) => {
+    res.send("Express Server");
+});
+app.listen(port, () => {
+    console.log(`[server]: Server is running at https://localhost:${port}`);
+});
