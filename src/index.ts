1b148d80cfb7db9d5e0b60e58755d7e94a26e69b
Mauricio Scain <scainmauricio@gmail.com>
2022-06-25 11:21:37 +0200

    gitIgnore

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import Express from "express";
+import Express, {Application, Request, Response} from "express";
 // import DotEnv from "dotenv";
 // import Cors from "cors";
 
-const app = Express();
+const app:Application = Express();
 const port = process.env.PORT || 3002;
 
-app.get("/", (req, res) => {
+app.get("/", (req:Request, res:Response) => {
   res.send("Express Server");
 });
 
