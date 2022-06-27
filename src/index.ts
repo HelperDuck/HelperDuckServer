b4739729ded7a1f1d8e7f87d2f5cecf4d75dcb33
noelcv <79803356+noelcv@users.noreply.github.com>
2022-06-27 12:07:49 +0200

    BE:Cors/socketIO setup

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,31 @@ import Express from 'express';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
 dotenv.config();
-import Cors from 'cors';
+import cors from 'cors';
 import router from './router/router';
 import http from 'http';
+import { Server } from 'socket.io';
 
 const PORT = process.env.PORT || 3002;
 
 const app = Express();
 app.use(morgan('dev'));
-app.use(Cors()).use(Express.json());
+app.use(cors()).use(Express.json());
 app.use(router);
 
+//HTTP server
 const server = http.createServer(app);
+//Websocket server
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+io.on("connection", () => {
+  console.log('user is connected')
+})
+
 
 server.listen(PORT, () => {
   console.log(`🦆🦆🦆 Server is running at http://localhost:${PORT} 🦆🦆🦆`);
