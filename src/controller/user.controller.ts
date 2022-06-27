efebf75e72784168e78c528b684fb53b4a3f3533
Siebe88 <siebe.kylstra@gmail.com>
2022-06-27 13:43:28 +0200

    API: Added user delete and post controller

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Technology, UsersToTechnologies } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export async function getUser(req: Request, res: Response) {
@@ -17,6 +17,8 @@ export async function getUser(req: Request, res: Response) {
         languages: { include: { language: true } },
       },
     });
+
+    if (!user) return res.status(404).send('User not found');
 
     //Added fields but needs to be queried for
     const userComplete = {
@@ -37,57 +39,111 @@ export async function getUser(req: Request, res: Response) {
 }
 
 //User related. Test for build // test
-export async function postUserProfile(req: Request, res: Response) {
+export async function createNewUser(req: Request, res: Response) {
   try {
     const {
-      firstName,
-      lastName,
+      uid,
       userName,
       email,
+      firstName,
+      lastName,
       userBio,
-      profilePic,
-      thecnologies,
-      languages,
       gitHubProfile,
-      openedRequests,
-      acceptedRequests,
-      avgTip,
-      rating,
+      profilePic,
+      technologies,
+      languages,
     } = req.body;
 
-    // const result = await mockModel.create({    //uncomment and replace mock model and sintaxe to add into DBase
-    const result = {
-      //mock, delete after model and database are done
-      firstName,
-      lastName,
-      userName,
-      email,
-      userBio,
-      profilePic,
-      thecnologies,
-      languages,
-      gitHubProfile,
-      openedRequests,
-      acceptedRequests,
-      avgTip,
-      rating,
-    };
-    res.send(result);
-    res.status(201);
+    //Check for neccessary fields
+    if (!uid || !userName || !email || !profilePic) {
+      res.send(
+        `Missing fields in request: ${!uid ? 'uid' : ''} ${
+          !userName ? 'userName' : ''
+        } ${!email ? 'email' : ''} ${!profilePic ? 'profilePic' : ''}`
+      );
+      return res.status(400);
+    }
+
+    //Check if no unqiue fields are double
+    const userExists = await prisma.user.findFirst({
+      where: { OR: [{ uid: uid }, { userName: userName }, { email: email }] },
+    });
+
+    if (userExists) {
+      res.send(`User already exists with: ${uid} ${userName} ${email}`);
+      return res.status(400);
+    }
+
+    //Create user
+    const user = await prisma.user.create({
+      data: {
+        uid: uid,
+        userName: userName,
+        email: email,
+        firstName: firstName,
+        lastName: lastName,
+        userBio: userBio,
+        gitHubProfile: gitHubProfile,
+        profilePic: profilePic,
+        technologies: {
+          create: technologies.map((tech: any) => {
+            return { technology: { connect: { name: tech.technology.name } } };
+          }),
+        },
+        languages: {
+          create: languages.map((lang: any) => {
+            return { language: { connect: { code: lang.language.code } } };
+          }),
+        },
+      },
+    });
+
+    res.send(user);
+    return res.status(201);
   } catch (err) {
     console.log('Error at postUserProfile Controller', err);
     res.sendStatus(400);
   }
 }
 
-export async function getUserProfile(req: Request, res: Response) {
+export async function deleteUser(req: Request, res: Response) {
   try {
-    // const user = await mockModel.findById(req.params.uid);  //uncomment and replace mock model and sintaxe to add into DBase
-    const user = req.params.uid; //mock, delete after model and database are done
-    res.status(200);
+    const uid = req.params.uid;
+
+    const user = await prisma.user.findUnique({
+      where: {
+        uid: uid,
+      },
+    });
+
+    if (!user) return res.status(404).send('User not found');
+
+    await prisma.usersToTechnologies.deleteMany({
+      where: {
+        user: {
+          id: user.id,
+        },
+      },
+    });
+
+    await prisma.usersToLanguages.deleteMany({
+      where: {
+        user: {
+          id: user.id,
+        },
+      },
+    });
+
+    await prisma.user.delete({
+      where: {
+        uid: uid,
+      },
+    });
+
     res.send(user);
+    res.status(200);
   } catch (err) {
-    console.log('Error at getUserProfile Controller ', err);
-    res.sendStatus(400);
+    console.log('Error at deleteUserProfile Controller', err);
+    return res.status(404).send('User not found');
   }
 }
