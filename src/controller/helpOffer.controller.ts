507c7a364073ef2be5d5177ee8a606f332629817
Siebe88 <siebe.kylstra@gmail.com>
2022-06-29 12:40:05 +0200

    HelpOffer: Added first methods

--- src/controller/helpOffer.controller.ts
+++ src/controller/helpOffer.controller.ts
@@ -1,2 +1,24 @@
 import { Request, Response } from 'express';
 import model from '../model/index';
+
+//HelpOffer related functions
+export async function getAllHelpOffers(req: Request, res: Response) {
+  const helpOffers = await model.helpOffer.getAllHelpOffers();
+  if (!helpOffers) return res.status(400).send('Error getting helpOffers');
+  return res.status(200).send(helpOffers);
+}
+
+export async function getHelpOfferById(req: Request, res: Response) {
+  const id: number = parseInt(req.params.id);
+  if (!id) return res.status(400).send('No id provided');
+  const helpOffer = await model.helpOffer.getHelpOfferById(id);
+  if (!helpOffer) return res.status(404).send('HelpOffer not found');
+  return res.status(200).send(helpOffer);
+}
+
+export async function createHelpOffer(req: Request, res: Response) {
+  const helpOfferData: any = req.body;
+
+  if (!helpOfferData.userId || !helpOfferData.subject)
+    return res.status(400).send('No userId or subject provided');
+}
--- src/model/helpOffer.model.ts
+++ src/model/helpOffer.model.ts
@@ -1,4 +1,4 @@
-import { HelpRequest, PrismaClient } from '@prisma/client';
+import { HelpOffer, HelpRequest, PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export async function getAllHelpOffers() {
@@ -13,6 +13,37 @@ export async function getAllHelpOffers() {
     return helpOffers;
   } catch (err) {
     console.log('Error at Model-getAllHelpOffers', err);
+    return null;
+  }
+}
+
+export async function getHelpOfferById(id: number) {
+  try {
+    const helpOffer = await prisma.helpOffer.findUnique({
+      where: {
+        id: id,
+      },
+      include: {
+        user: true,
+        helpRequest: true,
+        helpSession: true,
+      },
+    });
+    return helpOffer;
+  } catch (err) {
+    console.log('Error at Model-getHelpOfferById', err);
+    return null;
+  }
+}
+
+export async function createHelpOffer(helpOfferData: HelpOffer) {
+  try {
+    const helpOffer = await prisma.helpOffer.create({
+      data: helpOfferData,
+    });
+    return helpOffer;
+  } catch (err) {
+    console.log('Error at Model-createHelpOffer', err);
     return null;
   }
 }
