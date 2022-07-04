40c942cc2be1d0272386e82ba1a82f03e42e6e8f
noelcv <79803356+noelcv@users.noreply.github.com>
2022-07-04 18:51:41 +0200

    Payments: Stripe Setup

---
+++ src/controller/payment.controller.ts
@@ -0,0 +1,42 @@
+
+import dotenv from 'dotenv';
+
+import { Request, Response } from 'express';
+import { Stripe } from 'stripe';
+
+let SUPER_SECRET_KEY: string;
+if(process.env.SECRET_KEY) {
+  SUPER_SECRET_KEY = process.env.SECRET_KEY;
+} else {
+  throw new Error('SECRET_KEY not found');
+}
+
+const stripe = new Stripe(SUPER_SECRET_KEY, {
+  apiVersion: '2020-08-27',
+});
+
+export default async (req: Request, res: Response) => {
+  if (req.method === "POST") {
+    try {
+      const { amount } = req.body;
+      // Psst. For production-ready applications we recommend not using the
+      // amount directly from the client without verifying it first. This is to
+      // prevent bad actors from changing the total amount on the client before
+      // it gets sent to the server. A good approach is to send the quantity of
+      // a uniquely identifiable product and calculate the total price server-side.
+      // Then, you would only fulfill orders using the quantity you charged for.
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount,
+        currency: "eur"
+      });
+
+      res.status(200).send(paymentIntent.client_secret);
+    } catch (err) {
+      res.status(500).json({ statusCode: 500, message: err });
+    }
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
+  }
+};
