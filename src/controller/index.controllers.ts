cdb9822c2e61e186b44007e231ec5ac93904d57f
noelcv <79803356+noelcv@users.noreply.github.com>
2022-07-04 18:54:00 +0200

    Add Payment Controller to Index

--- src/controller/index.controllers.ts
+++ src/controller/index.controllers.ts
@@ -4,5 +4,6 @@ import * as languages from './language.controller';
 import * as helpRequest from './helpRequest.controller';
 import * as helpOffer from './helpOffer.controller';
 import * as helpReview from './helpReview.controller';
+import * as payment from './payment.controller';
 
-export default { user, helpRequest, technology, languages, helpOffer, helpReview };
+export default { user, helpRequest, technology, languages, helpOffer, helpReview, payment };
